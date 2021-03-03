868210e2b281e4aec0b0b1d30948e419118f79fd
anna871-coder <szepvolgyi.ann@gmail.com>
2021-03-03 13:32:36 +0100

    with React2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,44 +2,38 @@ import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
 
-async function upload() {
-  console.log("The Client is running");
-  /*    event.preventDefault(); */
-  console.log("We blocked the file upload process");
-
-  let formData = new FormData();
-
-  let file = document.getElementById("userfile");
-  let name = document.getElementById("username");
-  formData.append("userfile", file.files[0]);
-  formData.append("username", name.value);
-
-  for (let value of formData.values()) {
-    console.log(value);
-  }
-
-  await fetch("http://localhost:8000/upload", {
-    method: "POST",
-    body: formData,
-    mode: "no-cors",
-  })
-    .then((response) => response)
-    .then((result) => console.log("Success", result));
-}
-
 function App() {
   const [update, setUpdate] = useState(false);
 
   useEffect(() => {
-    if (update) upload();
-    setUpdate(false);
+    if (update) {
+      let formData = new FormData();
+
+      let file = document.getElementById("userfile");
+      let name = document.getElementById("username");
+      formData.append("userfile", file.files[0]);
+      formData.append("username", name.value);
+
+      for (let value of formData.values()) {
+        console.log(value);
+      }
+
+      fetch("http://localhost:8000/upload", {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+      })
+        .then((response) => response)
+        .then((result) => console.log("Success", result));
+      /* .then(setUpdate(false)) */
+    }
   }, [update]);
 
   return (
     <div className="App" id="uploadForm">
       <input type="file" id="userfile" />
       <input type="text" id="username" />
-      <button onClick={() => setUpdate(true)} value="Upload!">
+      <button onClick={() => setUpdate(!update)} value="Upload!">
         Upload
       </button>
     </div>
