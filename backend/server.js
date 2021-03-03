79c877836231114e93bbe5650936481364d77a18
anna871-coder <szepvolgyi.ann@gmail.com>
2021-03-03 13:32:24 +0100

    with React

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,47 @@ const app = express();
 const bodyParser = require("body-parser");
 var cors = require("cors");
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000/");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ),
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+const corsOptions = {
+  origin: "http://localhost:8000/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 const PORT = 8000;
-app.use("/form", express.static(__dirname + "../../frontend/index.html"));
-app.use("/pub", express.static(__dirname + "../../frontend/public"));
+/* app.use("/form", express.static(__dirname + "../../frontend/index.html"));
+app.use("/pub", express.static(__dirname + "../../frontend/public")); */
 
 // default options
 app.use(fileUpload());
 
-app.get("/ping", function (req, res) {
+/* app.get("/ping", function (req, res) {
   res.send("pong");
-});
+}); */
 
 app.post("/upload", function (req, res) {
   let sampleFile;
