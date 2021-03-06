8e87995ddfc93ae2debc2110153c07cb4506bd8f
anna871-coder <szepvolgyi.ann@gmail.com>
2021-03-06 18:55:10 +0100

    Send formData to server and save it

--- backend/server.js
+++ backend/server.js
@@ -2,59 +2,72 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
 const bodyParser = require("body-parser");
-var cors = require("cors");
-
-app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000/");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  ),
-    // Request headers you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+const cors = require("cors");
+const fs = require("fs");
+const PORT = 8000;
 const corsOptions = {
   origin: "http://localhost:8000/",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-
-const PORT = 8000;
-/* app.use("/form", express.static(__dirname + "../../frontend/index.html"));
-app.use("/pub", express.static(__dirname + "../../frontend/public")); */
-
-// default options
 app.use(fileUpload());
+let contents = fs.readFileSync("./uploads/data.json");
 
-/* app.get("/ping", function (req, res) {
-  res.send("pong");
-}); */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000/"); // Website you wish to allow to connect
+  res.setHeader(
+    // Request methods you wish to allow
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ),
+    res.setHeader(
+      // Request headers you wish to allow
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+  res.setHeader("Access-Control-Allow-Credentials", true); // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  next(); // Pass to next layer of middleware
+});
+
+//Json adatot parse-olni
+//kiválasztani a mail kulcsot,
+//kukacot, egyebeket rekurzív módon kiszedni
+//létrehozni egy fileelérési utat (mail alapján)
+//hozzáilleszteni a .json kiterjesztést
+//file-t írni
+//callback hiba
 
 app.post("/upload", function (req, res) {
   let sampleFile;
   let uploadPath;
-  let text;
+  let text = JSON.parse(req.body.userdata);
 
-  if (!req.files || Object.keys(req.files).length === 0) {
+  console.log(text);
+
+  let jsonContent = JSON.parse(contents);
+  console.log("jsonContent: " + jsonContent);
+  jsonContent.push(text);
+  let json = JSON.stringify(jsonContent);
+  console.log(json);
+  let filename = text.email.replace(/[^a-zA-Z0-9]/g, "");
+  console.log(filename);
+  fs.writeFile(
+    __dirname + "/uploads/" + `${filename}` + ".json",
+    json,
+    "utf8",
+    function () {}
+  );
+  if (!req.files) {
     res.status(400).send("No files were uploaded.");
     return;
   }
+
+  //kép feltöltés
+
   console.log("req.files >>>", req.files); // eslint-disable-line
   sampleFile = req.files.userfile;
   text = req.files.text;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,42 +2,105 @@ import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
   const [update, setUpdate] = useState(false);
 
-  useEffect(() => {
-    if (update) {
-      let formData = new FormData();
+  const upload = () => {
+    let formData = new FormData();
 
-      let file = document.getElementById("userfile");
-      let name = document.getElementById("username");
-      formData.append("userfile", file.files[0]);
-      formData.append("username", name.value);
-
-      for (let value of formData.values()) {
-        console.log(value);
-      }
-
-      fetch("http://localhost:8000/upload", {
-        method: "POST",
-        body: formData,
-        mode: "no-cors",
-      })
-        .then((response) => response)
-        .then((result) => console.log("Success", result));
-      /* .then(setUpdate(false)) */
+    const elements = document.querySelectorAll(".form-control");
+    let obj = {};
+    for (const elem of elements) {
+      obj[elem.id] = elem.value;
     }
+    let file = document.querySelector("#userfile");
+    formData.append("userdata", JSON.stringify(obj));
+    formData.append("userfile", file.files[0]);
+
+    for (let value of formData.values()) {
+      console.log(value);
+    }
+
+    fetch("http://localhost:8000/upload", {
+      method: "POST",
+      body: formData,
+      mode: "no-cors",
+    })
+      .then((response) => response)
+      .then((result) => console.log("Success", result));
+    /* .then(setUpdate(false)) */
+  };
+
+  //userdata stringify-olt data lesz, beletesszük az összes olyan tartalmat, amit el akarunk küldeni
+  //queryselectorall, for ciklis, input mező value-ja lesz
+
+  useEffect(() => {
+    upload();
   }, [update]);
 
   return (
-    <div className="App" id="uploadForm">
-      <input type="file" id="userfile" />
-      <input type="text" id="username" />
-      <button onClick={() => setUpdate(!update)} value="Upload!">
-        Upload
-      </button>
+    <div className="main-block">
+      <h1>Personal Data</h1>
+      <div className="main" id="uploadForm">
+        <input type="file" id="userfile" />
+        <input type="text" id="username" />
+      </div>
+      <div>
+        <div className="info main">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Name"
+            id="name-address"
+          />
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder="Email"
+            id="email"
+          />
+          <input
+            type="zip"
+            className="form-control"
+            placeholder="Zip code"
+            id="zip"
+          />
+          <input
+            type="city"
+            className="form-control"
+            placeholder="City"
+            id="city"
+          />
+          <input
+            type="street"
+            className="form-control"
+            placeholder="Street"
+            id="street"
+          />
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Number"
+            id="number"
+          />
+          <textarea
+            className="form-control"
+            rows="4"
+            id="other"
+            placeholder="Other"
+          ></textarea>
+
+          <button
+            onClick={() => setUpdate(!update ? true : false)}
+            value="Upload!"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
